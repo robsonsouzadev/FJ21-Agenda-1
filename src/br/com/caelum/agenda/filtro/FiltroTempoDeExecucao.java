0b559cbb3f954164459869244fcec6a6189c10ae
Fábio Ueno <fabio.t.ueno@gmail.com>
2016-08-25 18:26:10 -0300

    [Exercise 10.2] Optionals - Filter to measure time
    Filter created to measure all execution times.

---
+++ src/br/com/caelum/agenda/filtro/FiltroTempoDeExecucao.java
@@ -0,0 +1,35 @@
+package br.com.caelum.agenda.filtro;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter("/*")
+public class FiltroTempoDeExecucao implements Filter {
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		Long tempoInicial = System.currentTimeMillis();
+		chain.doFilter(request, response);
+		Long tempoFinal = System.currentTimeMillis();
+
+		String uri = ((HttpServletRequest) request).getRequestURI();
+		String parametros = ((HttpServletRequest) request).getParameter("logica");
+
+		System.out.println("Tempo da requisição de " + uri + "?logica=" + parametros + " demorou (ms): " + (tempoFinal - tempoInicial));
+	}
+
+	@Override
+	public void destroy() {}
+}
